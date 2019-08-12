323ec9cafc652664edbf887d719b1761094953af
Joan Pujols <joanpujols1998@gmail.com>
2019-08-11 22:30:00 -0400

    Initial commit, already started with backend for the project using Node/Express

---
+++ dbschema.js
@@ -0,0 +1,46 @@
+let db = {
+  users: [
+  {
+      userId: 'eCjAD2y6tHdIbntg3IHC64dmmz62',
+      email: 'work@email.com',
+      handle: 'user',
+      createdAt: '2019-08-12T00:29:50.964Z',
+      imageUrl: 'image/dasdasdas/asdasasas',
+      bio: 'Hello my name is user, nice to meet you',
+      website: 'https://user.com',
+      location: 'New york, USA'
+  }
+],
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: "2019-08-10T17:12:57.100Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: 'eCjAD2y6tHdIbntg3IHC64dmmz62',
+    email: 'work@email.com',
+    handle: 'user',
+    createdAt: '2019-08-12T00:29:50.964Z',
+    imageUrl: 'image/dasdasdas/asdasasas',
+    bio: 'Hello my name is user, nice to meet you',
+    website: 'https://user.com',
+    location: 'New york, USA'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'l3S7dOLsCqsYscQowdoK'
+    },
+    {
+      userHandle: 'user',
+      screamId: 'lAJ261NHGdtH6PWEHELa'
+    }
+  ]
+}
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyDNaSGU9q61gMnq6Ut8JqL02yzvW5CrU80",
+  authDomain: "socialape-5fb9d.firebaseapp.com",
+  databaseURL: "https://socialape-5fb9d.firebaseio.com",
+  projectId: "socialape-5fb9d",
+  storageBucket: "socialape-5fb9d.appspot.com",
+  messagingSenderId: "332233805552",
+  appId: "1:332233805552:web:eecefcccf472bf19"
+};
